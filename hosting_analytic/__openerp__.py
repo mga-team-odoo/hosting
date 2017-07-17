7f2f75cfbddef51245be5abf71099bf8e0113ccc
Christophe CHAUVET <christophe.chauvet@gmail.com>
2017-07-17 22:22:23 +0200

    Modules structure for analytic hosting

---
+++ hosting_analytic/__openerp__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2016-2017 MIROUNGA <http://mirounga.fr>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "Analytic account for hosting",
+    'version': "8.0.1.0.0",
+    'author': "Mirounga, Christophe CHAUVET",
+    'license': "AGPL-3",
+    'website': "http://www.mirounga.fr",
+    'category': "Accounting & Finance",
+    'depends': [
+        'base',
+        'analytic',
+    ],
+    'data': [
+    ],
+    'application': False,
+    'installable': True,
+    # 'pre_init_hook': 'pre_init_hook',
+}
+
